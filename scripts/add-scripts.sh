58afed3c8749876a03e7a62a53a5ca3356335878
Ming Liu <liu.ming50@gmail.com>
2017-11-12 11:51:39 +0800

    add-scripts.sh: fix a wrong path
    Signed-off-by: Ming Liu <liu.ming50@gmail.com>

--- scripts/add-scripts.sh
+++ scripts/add-scripts.sh
@@ -5,7 +5,7 @@ THIS_DIR=$(dirname $0)
 
 for script in $EXPORT_SCRIPTS; do
     if [ -x $THIS_DIR/$script ]; then
-        mkdir -p $THIS_DIR/../scripts
-        ln -srfT $THIS_DIR/$script $THIS_DIR/../scripts/$script
+        mkdir -p $THIS_DIR/../../../scripts
+        ln -srfT $THIS_DIR/$script $THIS_DIR/../../../scripts/$script
     fi
 done
